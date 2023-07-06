9a71a4a5f2ae14de3d0c541207df5d5374c6ceb4
jvr0123 <ragdejorge@gmail.com>
2023-07-05 22:02:42 -0700

    Solutions updated

--- epi_judge_cpp/count_bits.cc
+++ epi_judge_cpp/count_bits.cc
@@ -1,8 +1,12 @@
 #include "test_framework/generic_test.h"
 
 short CountBits(unsigned int x) {
-  // TODO - you fill in here.
-  return 0;
+  short n = 0;
+  while(x){
+    n += x & 1;
+    x >>= 1;
+  }
+  return n;
 }
 
 int main(int argc, char* argv[]) {
