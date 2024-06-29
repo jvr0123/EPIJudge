e4421f6ef248463328f9affadec0e53641ead3d3
Jorge Ragde <ragdejorge@gmail.com>
2024-06-28 18:41:29 -0700

    [Linked List] Test Whether Singly Linked List is Palindromic

--- epi_judge_cpp/is_list_palindromic.cc
+++ epi_judge_cpp/is_list_palindromic.cc
@@ -1,15 +1,42 @@
 #include "list_node.h"
 #include "test_framework/generic_test.h"
 
+shared_ptr<ListNode<int>> reverseList(shared_ptr<ListNode<int>> head) {
+    shared_ptr<ListNode<int>> prev{nullptr}, next{nullptr};
+    auto curr{head};
+    while (curr) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
 bool IsLinkedListAPalindrome(shared_ptr<ListNode<int>> L) {
-  // TODO - you fill in here.
-  return true;
+    if (!L)
+        return true;
+    auto slow{L}, fast{L};
+    while(fast && fast ->next){
+      slow = slow -> next;
+      fast = fast->next->next;
+    }
+    auto reverse{reverseList(slow)};
+    while (reverse && L && L != reverse) {
+        if (reverse->data != L->data)
+            return false;
+        reverse = reverse->next, L = L->next;
+    }
+    return true;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"L"};
-  return GenericTestMain(args, "is_list_palindromic.cc",
-                         "is_list_palindromic.tsv", &IsLinkedListAPalindrome,
-                         DefaultComparator{}, param_names);
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"L"};
+    return GenericTestMain(args,
+                           "is_list_palindromic.cc",
+                           "is_list_palindromic.tsv",
+                           &IsLinkedListAPalindrome,
+                           DefaultComparator{},
+                           param_names);
 }
--- problem_mapping.js
+++ problem_mapping.js
@@ -816,7 +816,7 @@ problem_mapping = {
         },
         "7.10 Implement even-odd merge": {
             "C++: even_odd_list_merge.cc": {
-                "passed": 1015,
+                "passed": 1,
                 "total": 1015
             },
             "Java: EvenOddListMerge.java": {
@@ -830,7 +830,7 @@ problem_mapping = {
         },
         "7.11 Test whether a singly linked list is palindromic": {
             "C++: is_list_palindromic.cc": {
-                "passed": 0,
+                "passed": 203,
                 "total": 203
             },
             "Java: IsListPalindromic.java": {
