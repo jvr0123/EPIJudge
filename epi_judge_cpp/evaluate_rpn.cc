2b6c1626fdb2d14750e66a1adbac75379238baa8
jvr0123 <ragdejorge@gmail.com>
2024-07-03 15:30:45 -0700

    [Stacks and Queues]  Evaluate RPN Expressions

--- epi_judge_cpp/evaluate_rpn.cc
+++ epi_judge_cpp/evaluate_rpn.cc
@@ -3,13 +3,35 @@
 #include "test_framework/generic_test.h"
 using std::string;
 int Evaluate(const string& expression) {
-  // TODO - you fill in here.
-  return 0;
+    std::stack<int> results{};
+    std::stringstream ss{expression};
+    const char delimiter{','};
+    std::string token;
+    const std::unordered_map<std::string, std::function<int(int, int)>> operators{ 
+      {"+", [](int x, int y) -> int {return x + y;}},
+      {"-", [](int x, int y) -> int {return x - y;}},
+      {"*", [](int x, int y) -> int {return x * y;}},
+      {"/", [](int x, int y) -> int {return x / y;}}
+    };
+    while(std::getline(ss, token, delimiter)){
+      if(operators.count(token)){
+        const int y = results.top();
+        results.pop();
+        const int x = results.top();
+        results.pop();
+        results.push(operators.at(token)(x, y));
+      }
+      else{
+        results.push(std::stoi(token));
+      }
+
+    }
+    return results.top();
+    
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"expression"};
-  return GenericTestMain(args, "evaluate_rpn.cc", "evaluate_rpn.tsv", &Evaluate,
-                         DefaultComparator{}, param_names);
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"expression"};
+    return GenericTestMain(args, "evaluate_rpn.cc", "evaluate_rpn.tsv", &Evaluate, DefaultComparator{}, param_names);
 }
