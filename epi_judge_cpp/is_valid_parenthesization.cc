f2f9276a89c32f974c856c170c8737b92ea6dc77
jvr0123 <ragdejorge@gmail.com>
2024-07-03 16:24:02 -0700

    [Stacks and Queues]  Is a String Well-Formed?

--- epi_judge_cpp/is_valid_parenthesization.cc
+++ epi_judge_cpp/is_valid_parenthesization.cc
@@ -3,14 +3,28 @@
 #include "test_framework/generic_test.h"
 using std::string;
 bool IsWellFormed(const string& s) {
-  // TODO - you fill in here.
-  return true;
+    std::stack<char> leftChars{};
+    const std::unordered_map<char, char> parenthesis{{'(', ')'}, {'[', ']'}, {'{', '}'}};
+    for (const auto& c : s) {
+        if (parenthesis.count(c))
+            leftChars.emplace(c);
+        else {
+            if (leftChars.empty() || parenthesis.at(leftChars.top()) != c) {
+                return false;
+            }
+            leftChars.pop();
+        }
+    }
+    return leftChars.empty();
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"s"};
-  return GenericTestMain(args, "is_valid_parenthesization.cc",
-                         "is_valid_parenthesization.tsv", &IsWellFormed,
-                         DefaultComparator{}, param_names);
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"s"};
+    return GenericTestMain(args,
+                           "is_valid_parenthesization.cc",
+                           "is_valid_parenthesization.tsv",
+                           &IsWellFormed,
+                           DefaultComparator{},
+                           param_names);
 }
