fdb361fb77af1e43b16f3c97f59358582ae0f0cd
jvr0123 <ragdejorge@gmail.com>
2024-07-25 19:49:44 -0700

    [Stacks and Queues] Compute Buildings With a Sunset View

--- epi_judge_cpp/sunset_view.cc
+++ epi_judge_cpp/sunset_view.cc
@@ -1,13 +1,27 @@
 #include <iterator>
 #include <vector>
+#include <algorithm>
 
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<int> ExamineBuildingsWithSunset(
     vector<int>::const_iterator sequence_begin,
     const vector<int>::const_iterator& sequence_end) {
-  // TODO - you fill in here.
-  return {};
+  std::vector<std::pair<int, int>> monoStack{};
+  std::vector<int> res{};
+  auto buildingIdx{0};
+  while(sequence_begin != sequence_end){
+    while(!monoStack.empty() && *sequence_begin >= monoStack.back().second){
+      monoStack.pop_back();
+    }
+    monoStack.push_back(std::make_pair(buildingIdx, *sequence_begin));
+    ++sequence_begin, ++buildingIdx;
+  }
+  for(const auto& pa : monoStack){
+    res.emplace_back(pa.first);
+  }
+  std::reverse(res.begin(), res.end());
+  return res;
 }
 vector<int> ExamineBuildingsWithSunsetWrapper(const vector<int>& sequence) {
   return ExamineBuildingsWithSunset(cbegin(sequence), cend(sequence));
