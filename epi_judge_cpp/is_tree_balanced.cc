5743f531a818988b7e1f6ffb897f21af8ccb1180
jvr0123 <ragdejorge@gmail.com>
2024-07-30 16:38:13 -0700

    [Binary Trees] Test if a Binary Tree is Height-Balanced

--- epi_judge_cpp/is_tree_balanced.cc
+++ epi_judge_cpp/is_tree_balanced.cc
@@ -1,9 +1,23 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
-bool IsBalanced(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+
+struct heightBalance{
+  int height{};
+  bool balance{true};
+};
+
+heightBalance depth(const unique_ptr<BinaryTreeNode<int>>& tree){
+  if(!tree)
+    return {0, true};
+  auto left = depth(tree->left);
+  auto right = depth(tree->right); 
+  return {std::max(left.height, right.height) + 1, left.balance && right.balance && std::abs(left.height - right.height) <= 1};
 }
+
+bool IsBalanced(const unique_ptr<BinaryTreeNode<int>>& tree) {
+  return depth(tree).balance; 
+}
+
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
